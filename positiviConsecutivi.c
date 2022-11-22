853fb076865a5f4aed63e10bea8e4ec0061bd64d
gabriproo <76819397+gabriproo@users.noreply.github.com>
2022-11-22 14:41:42 +0100

    Update positiviConsecutivi.c

--- positiviConsecutivi.c
+++ positiviConsecutivi.c
@@ -3,12 +3,12 @@
 int main() {
     int a, b = -1, pp = 0;
 
-    do {
+     while (a != 0 || b != 0) {
         printf("Inserisci numero: ");
         scanf("%d", &a);
         if (a > 0 && b > 0) pp = 1;
         b = a;
-    } while (a != 0 || b != 0);
+    }
     if (pp == 1) printf("Sono stati inseriti almeno due positivi consecutivamente");
     else printf("Non sono stati inseriti due positivi consecutivamente");
 }
