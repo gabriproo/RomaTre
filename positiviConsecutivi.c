7ab3d82f8be59451cd62e5b787954a7396488ee7
gabriproo <76819397+gabriproo@users.noreply.github.com>
2022-11-22 14:32:25 +0100

    Almeno due positivi consecutivi

---
+++ positiviConsecutivi.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int pp = 0, a, b = -1, c;
+
+    do {
+        printf("Inserisci numero: ");
+        scanf("%d", &a);
+        if (a > 0 && b > 0) pp = 1;
+        b = a;
+    } while (a != 0 || b != 0);
+    if (pp == 1) printf("Sono stati inseriti almeno due positivi consecutivamente");
+    else printf("Non sono stati inseriti due positivi consecutivamente");
+}
